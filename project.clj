82fa917948652740d1727304cd249349167e5757
Roman Pearah <github.to.soma@neverbox.com>
2016-08-28 15:21:11 -0400

    Update dependencies and sync version with Onyx

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject onyx-rabbitmq "0.7.0-SNAPSHOT"
+(defproject onyx-rabbitmq "0.9.9.0"
   :description "Onyx plugin for rabbitmq"
   :url "FIX ME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.onyxplatform/onyx "0.7.14"]
-                 [com.novemberain/langohr "3.3.0"]
-                 [less-awful-ssl "1.0.0"]
-                 [environ "1.0.0"]]
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.onyxplatform/onyx "0.9.9"]
+                 [com.novemberain/langohr "3.6.1"]
+                 [less-awful-ssl "1.0.1"]
+                 [environ "1.1.0"]]
   :plugins [[lein-environ "1.0.0"]]
   :profiles {:dev {:dependencies []
-                   :plugins []}})
+                   :plugins      []}})
