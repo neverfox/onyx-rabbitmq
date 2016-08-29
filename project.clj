2efd1e4356fcf6c12aec1c86592758fe3d9f407c
Roman Pearah <github.to.soma@neverbox.com>
2016-08-29 11:22:13 -0400

    Add additional dependencies

--- project.clj
+++ project.clj
@@ -1,13 +1,18 @@
 (defproject onyx-rabbitmq "0.9.9.0"
-  :description "Onyx plugin for rabbitmq"
-  :url "FIX ME"
+  :description "Onyx plugin for RabbitMQ"
+  :url "https://github.com/neverfox/onyx-rabbitmq"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.onyxplatform/onyx "0.9.9"]
+  :dependencies [[aero "1.0.0"]
+                 [cheshire "5.6.3"]
+                 [com.cognitect/transit-clj "0.8.288"]
                  [com.novemberain/langohr "3.6.1"]
+                 [im.chit/hara.component "2.3.6"]
                  [less-awful-ssl "1.0.1"]
-                 [environ "1.1.0"]]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.onyxplatform/onyx "0.9.9"]
+                 [prismatic/schema "1.1.3"]]
   :plugins [[lein-environ "1.0.0"]]
-  :profiles {:dev {:dependencies []
-                   :plugins      []}})
+  :profiles {:dev {:plugins [[lein-set-version "0.4.1"]
+                             [lein-update-dependency "0.1.2"]
+                             [lein-pprint "1.1.1"]]}})
